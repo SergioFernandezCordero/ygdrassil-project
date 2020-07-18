70a5bab33e513b92bbba567a1106de4de278467b
El Autoestopista <elautoestopista@proyectopqmc.com>
2020-07-19 00:58:10 +0200

    Add docker push to build.sh script

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #
-docker build --pull --no-cache -t elautoestopista/jenkins .
+docker build --pull --no-cache -t elautoestopista/jenkins . && docker push elautoestopista/jenkins:latest
